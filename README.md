182edf9092917ca9da43a9f3b90e493cb181d4e9
Jayden Bailey <jaydenkieran@gmail.com>
2026-02-01 19:11:54 +0000

    Support reading word list from a wiki page

--- README.md
+++ README.md
@@ -4,8 +4,16 @@ MediaWiki extension which allows you to define a word list that is used for crea
 
 ## Configuration
 ```php
-// Configure the extension
-$wgTempUserWordNamesList = [ 'apple', 'pear', 'banana', 'pineapple', 'mango', ... ];
+/**
+* To configure a list of words, $wgTempUserWordNamesList can be set. It should have either a 'pages' or 'words' key.
+*
+* If 'page' is provided, it should contain a single string that points to a wiki page containing a list of words.
+* By default, the extension uses 'MediaWiki:TempUserWordList'. Each word should be separated by a new line.
+* $wgTempUserWordNamesCentralWiki can be set to a different wiki that uses the same shared user table to fetch words from there instead.
+*
+* If 'words' is provided, it should contain an array of strings.
+*/
+$wgTempUserWordNamesList['page'] = 'MediaWiki:TempUserWordList';
 $wgTempUserWordNamesLength = 3;
 
 // Ensure that MediaWiki uses the extension
@@ -13,5 +21,11 @@ $wgAutoCreateTempUser['serialMapping']['type'] = 'words';
 ```
 
 Try and use a lot of words to ensure that a lot of combinations are possible. 500 is more than enough. There is no possibility of collision even with a small number of words, as the numerical index is inserted at the end of the username.
+
+### Troubleshooting
+
+If there any problems with word list, the extension will log them to the channel `TempUserWordNames` and use a default list of words instead.
+
+### Risk of collisions
 
 This extension makes temporary account usernames more readable while still ensuring there is no collision by appending the index at the end. However, if you'd like to turn off the indexes and risk collision, you can set `$wgTempUserWordNamesUseIndex = false;`.
--- src/TempUserWordNamesSerialMapping.php
+++ src/TempUserWordNamesSerialMapping.php
@@ -4,34 +4,125 @@ namespace MediaWiki\Extension\TempUserWordNames;
 
 use InvalidArgumentException;
 use MediaWiki\Config\Config;
+use MediaWiki\Content\TextContent;
+use MediaWiki\DAO\WikiAwareEntity;
+use MediaWiki\Logger\LoggerFactory;
+use MediaWiki\MainConfigNames;
+use MediaWiki\Page\PageStoreFactory;
+use MediaWiki\Revision\RevisionStoreFactory;
+use MediaWiki\Revision\SlotRecord;
 use MediaWiki\User\TempUser\SerialMapping;
+use Psr\Log\LoggerInterface;
+use WANObjectCache;
+use Wikimedia\LightweightObjectStore\ExpirationAwareness;
 
 class TempUserWordNamesSerialMapping implements SerialMapping {
-	private int $offset;
+    private LoggerInterface $logger;
+    private Config $config;
+    private WANObjectCache $objectCache;
+    private RevisionStoreFactory $revisionStoreFactory;
+	private PageStoreFactory $pageStoreFactory;
 
-	private int $numWords;
+    /** @var string[] */
+    private array $defaultWords = [
+        'Apple', 'Banana', 'Cherry', 'Grape', 'Peach', 'Pear', 'Strawberry', 'Watermelon', 'Apricot', 'Blueberry',
+        'Orange', 'Tomato', 'Plum', 'Lime', 'Lemon', 'Bread', 'Egg', 'Fish', 'Garlic', 'Sugar', 'Bagel', 'Tofu',
+        'Muffin', 'Cake', 'Perfect', 'Cheerful', 'Generous', 'Friendly', 'Happy', 'Important', 'Great', 'Real',
+        'Strong', 'Delighted', 'Merry', 'Sunny', 'Jovial', 'Elated', 'Lucky', 'Golden', 'Blissful', 'Pretty',
+        'Silly', 'Red', 'Yellow', 'Green', 'Blue', 'Orange', 'Purple', 'Pink', 'Cyan', 'Magenta', 'Fluorescent'
+    ];
 
-	private array $words = [];
+    private int $offset;
+    private int $numWords;
+    private array $words = [];
+    private bool $useIndex;
 
-	private bool $useIndex;
+    public function __construct(
+        array $config,
+        Config $mainConfig,
+        WANObjectCache $objectCache,
+        RevisionStoreFactory $revisionStoreFactory,
+		PageStoreFactory $pageStoreFactory
+    ) {
+        $this->logger = LoggerFactory::getInstance( 'TempUserWordNames' );
+        $this->config = $mainConfig;
+        $this->objectCache = $objectCache;
+        $this->revisionStoreFactory = $revisionStoreFactory;
+		$this->pageStoreFactory = $pageStoreFactory;
 
-	public function __construct( array $config, Config $mainConfig ) {
-		$this->offset = $config['offset'] ?? 0;
-		$this->numWords = $mainConfig->get( 'TempUserWordNamesLength' );
-		$this->words = $mainConfig->get( 'TempUserWordNamesList' );
-		$this->useIndex = $mainConfig->get( 'TempUserWordNamesUseIndex' );
-	}
+        $this->offset = $config['offset'] ?? 0;
+        $this->numWords = $mainConfig->get( 'TempUserWordNamesLength' );
+        $this->words = $this->getWordList();
+        $this->useIndex = $mainConfig->get( 'TempUserWordNamesUseIndex' );
+    }
 
-	public function getSerialIdForIndex( int $index ): string {
-		if ( empty( $this->words ) || $this->numWords <= 0 || $this->numWords > count( $this->words ) ) {
-			throw new InvalidArgumentException(
-				'The word list is empty, or the number of words is less than 1 or the length of the word list.' );
-		}
+    public function getWordList(): array {
+        if ( !empty( $this->words ) ) {
+            return $this->words;
+        }
 
-		$selected = array_map( function ( $k ) {
-			return ucfirst( $this->words[$k] );
-		}, array_rand( $this->words, $this->numWords ) );
-		shuffle( $selected );
-		return implode( '', $selected ) . ( $this->useIndex ? $index + $this->offset : '' );
-	}
+        $listConfig = $this->config->get( 'TempUserWordNamesList' );
+        if ( !$listConfig ) {
+            throw new InvalidArgumentException( '$wgTempUserNamesList must be defined!' );
+        }
+
+        $words = [];
+
+        if ( isset( $listConfig[ 'words' ] ) ) {
+            $words = $listConfig[ 'words' ];
+        } else if ( isset( $listConfig[ 'page' ] ) ) {
+            $pageName = $listConfig[ 'page' ];
+            $centralWiki = $this->config->get( 'TempUserWordNamesCentralWiki' );
+
+            $words = $this->objectCache->getWithSetCallback(
+                $this->objectCache->makeGlobalKey( 'tempuserwordnames', 'words' ),
+                ExpirationAwareness::TTL_HOUR,
+                function () use ( $pageName, $centralWiki ) {
+					// Note: we have no idea what the remote namespaces are at this point, so hopefully they match ours
+					$centralWikiIsCurrentWiki = $centralWiki === $this->config->get( MainConfigNames::DBname );
+					$page = $this->pageStoreFactory
+						->getPageStore( $centralWikiIsCurrentWiki ? WikiAwareEntity::LOCAL : $centralWiki )
+						->getPageByText( $pageName );
+                    $rev = $this->revisionStoreFactory
+						->getRevisionStore($centralWikiIsCurrentWiki ? WikiAwareEntity::LOCAL : $centralWiki )
+                        ->getRevisionByTitle( $page );
+                    $content = $rev?->getContent( SlotRecord::MAIN );
+                    if ( !$content ) {
+                        $this->logger->warning( "No main slot on configured wiki page: $pageName" );
+                        return false;
+                    }
+
+                    $text = ( $content instanceof TextContent ) ? $content->getText() : '';
+                    if ( !$text ) {
+                        $this->logger->warning( "Empty content on configured wiki page: $pageName" );
+                        return false;
+                    }
+
+                    return array_map( 'trim', explode( "\n", $text ) );
+                }
+            );
+
+            if ( empty( $words ) ) {
+                $this->logger->warning( "Configured word list is empty. Using fallback list." );
+                $words = $this->defaultWords;
+            }
+        }
+
+        if ( $this->numWords <= 0 || $this->numWords > count( $words ) ) {
+            $this->logger->warning( '$wgTempUserWordNamesLength is less than 1 or more than the length of the list.' .
+                ' Using fallback list.' );
+            $words = $this->defaultWords;
+        }
+
+        $this->words = $words;
+        return $words;
+    }
+
+    public function getSerialIdForIndex( int $index ): string {
+        $selected = array_map( function ( $k ) {
+            return ucfirst( $this->words[ $k ] );
+        }, array_rand( $this->words, $this->numWords ) );
+        shuffle( $selected );
+        return implode( '', $selected ) . ( $this->useIndex ? $index + $this->offset : '' );
+    }
 }
